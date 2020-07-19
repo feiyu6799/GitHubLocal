2b3939589569e011ef341492b8ea2dda9b721c17
feiyu6799 <571315408@qq.com>
2020-07-19 23:00:20 +0800

    引入配置文件

--- tank01/src/com/feiyu/tank/Main.java
+++ tank01/src/com/feiyu/tank/Main.java
@@ -8,11 +8,15 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		TankFrame tf = new TankFrame();
 		
+		int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));//坦克的数量
+
+		
 		//初始化敌方坦克
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<initTankCount; i++) {
 			tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN, Group.BAD, tf));
 		}
-		//new Thread(()->new Audio("audio/war1.wav").loop()).start();//声音
+		
+		new Thread(()->new Audio("audio/war1.wav").loop()).start();//声音
 
 		while(true) {
 			Thread.sleep(25);
