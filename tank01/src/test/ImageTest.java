0077e826a936431c55c8bc45c9db8a5c9f6a489e
feiyu6799 <571315408@qq.com>
2020-08-08 21:08:11 +0800

    注释掉初始化敌方坦克，为网络版做准备

--- tank01/src/test/ImageTest.java
+++ tank01/src/test/ImageTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.Socket;
 
 import javax.imageio.ImageIO;
 
@@ -11,8 +12,8 @@ import org.junit.Test;
 
 public class ImageTest {
 
-	@Test
-	public void test() {
+	
+/*	public void test() {
 		
 		BufferedImage image2;
 		try {
@@ -22,6 +23,25 @@ public class ImageTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}*/
+	@Test
+	public void test02(){
+        
+        try {
+        	Socket s = new Socket("127.0.0.1", 8888);
+			s.getOutputStream().write("HelloServer".getBytes());
+			s.getOutputStream().flush();
+	        //s.getOutputStream().close();
+	        System.out.println("write over, waiting for msg back...");
+	        byte[] bytes = new byte[1024];
+	        int len = s.getInputStream().read(bytes);
+	        System.out.println(new String(bytes, 0, len));
+	        s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
 	}
 
 }
